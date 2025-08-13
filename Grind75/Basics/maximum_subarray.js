895d244b6f8d59c6c5d3ebbfae9a1b4dd48ced4c
Akari Bowens <akarimbowens@gmail.com>
2025-08-12 23:07:28 -0700

    Update maximum_subarray.js

--- Grind75/Basics/maximum_subarray.js
+++ Grind75/Basics/maximum_subarray.js
@@ -20,3 +20,16 @@ var maxSubArray = function(nums) {
     
     return largestSum;
 };
+
+//Notes:
+
+/* 
+1. The first thing to do is set largestSum and the windowTotal to the first index of nums
+2. The next is to decide whether to keep the current value of windowTotal and add the number at the new index or to reset the windowTotal to the current value at num[i]
+3. Check if the window total is greater than the current largestSum
+-- Exit Loop --
+4. Return largesSum
+
+
+
+*/
